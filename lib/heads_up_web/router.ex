736cdce116357e544c36bf5672ecd969d5ba36bb
Francesc Leveque <francesc@leveque.es>
2025-07-01 18:04:11 +0200

    Chapter 6: Route Parameters. Tips show action

--- lib/heads_up_web/router.ex
+++ lib/heads_up_web/router.ex
@@ -20,6 +20,7 @@ defmodule HeadsUpWeb.Router do
 
     get "/", PageController, :home
     get "/tips", TipController, :index
+    get "/tips/:id", TipController, :show
   end
 
   def snoop(conn, _opts) do
