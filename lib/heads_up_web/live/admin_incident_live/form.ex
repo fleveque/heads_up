0212349b4b917ed441d9fc3462272af72a1da9f1
Francesc Leveque <francesc@leveque.es>
2025-07-08 13:25:15 +0000

    Chapter 34: Forms with Changesets

--- lib/heads_up_web/live/admin_incident_live/form.ex
+++ lib/heads_up_web/live/admin_incident_live/form.ex
@@ -1,14 +1,17 @@
 defmodule HeadsUpWeb.AdminIncidentLive.Form do
   use HeadsUpWeb, :live_view
 
+  alias HeadsUp.Incidents.Incident
   alias HeadsUp.Incidents
   alias HeadsUp.Admin
 
   def mount(_params, _session, socket) do
+    changeset = Incident.changeset(%Incident{}, %{})
+
     socket =
       socket
       |> assign(:page_title, "New Incident")
-      |> assign(:form, to_form(%{}, as: "incident"))
+      |> assign(:form, to_form(changeset))
 
     {:ok, socket}
   end
@@ -44,14 +47,22 @@ defmodule HeadsUpWeb.AdminIncidentLive.Form do
   end
 
   def handle_event("save", %{"incident" => incident_params}, socket) do
-    _incident = Admin.create_incident(incident_params)
+    case Admin.create_incident(incident_params) do
+      {:ok, _incident} ->
+        socket =
+          socket
+          |> put_flash(:info, "Incident created successfully.")
+          |> push_navigate(to: ~p"/admin/incidents")
 
-    socket =
-      push_navigate(
-        socket,
-        to: ~p"/admin/incidents"
-      )
+        {:noreply, socket}
 
-    {:noreply, socket}
+      {:error, %Ecto.Changeset{} = changeset} ->
+        socket =
+          socket
+          |> put_flash(:error, "Failed to create incident.")
+          |> assign(:form, to_form(changeset))
+
+        {:noreply, socket}
+    end
   end
 end
